33ed50f38156bc1657c706ff4692aaa69b7a43fc
jamesxu1288 <jamesxu1288@Gmail.com>
2022-12-31 01:20:14 -0500

    added hide/show button to password input field

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import cn from "classnames"
 import { FieldError } from "react-hook-form"
+import { BiShow, BiHide } from "react-icons/bi"
 
 interface InputProps {
    label: string
@@ -24,10 +25,13 @@ export default function Input({
    onChange,
 }: InputProps) {
    const [val, setVal] = useState(value)
+   const [visPwd, setVisPwd] = useState(false)
+   const [pwd, setPwd] = useState("")
 
    return (
       <div className="text-left">
          <p className="text-sm text-noir-300 font-regular pb-2">{label}</p>
+         <div style = {{display: "flex", flexDirection: "row"}}>
          <input
             className={cn(
                "border-neo-gray-800 bg-noir-900 outline-none text-neutral-400 placeholder:text-noir-500 w-full p-2 rounded focus:outline-blue-500 outline-offset-0 border focus:border-light-background-border transition-all duration-125 ease-in-out",
@@ -36,11 +40,12 @@ export default function Input({
                }
             )}
             placeholder={placeholder}
-            type={type}
+            type={visPwd ? "text" : "password"}
             value={val}
             required={required}
             disabled={disabled}
             onChange={(event: any) => {
+               setPwd(event.target?.value)
                const target = event.target as HTMLInputElement
                if (target) {
                   setVal(target.value)
@@ -48,6 +53,16 @@ export default function Input({
                }
             }}
          />
+         { type === "password" && 
+            <button
+               style = {{marginLeft: "-30px"}}
+               onClick = {() => setVisPwd(!visPwd)}
+               className = "text-white"
+            >
+               {visPwd ? <BiHide /> : <BiShow />}
+            </button>
+         }
+         </div>
          <p className="mt-1 text-red-500 text-sm">{error?.message}</p>
       </div>
    )
